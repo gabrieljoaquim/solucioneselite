e3c804936232ebdc5d99b2999138ef058d4f06ff
gabrieljoaquim <70859332+gabrieljoaquim@users.noreply.github.com>
2025-05-11 22:42:31 -0500

    se modifico en .env y se establecio la conecion

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // MongoDB Connection
+mongoose.set('strictQuery', true); // Suppress deprecation warning
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,

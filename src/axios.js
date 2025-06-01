48819b536c176fa18b83c55ddc7d4b7251d31b8d
gabrieljoaquim <70859332+gabrieljoaquim@users.noreply.github.com>
2025-05-31 21:16:04 -0500

    actualisado

---
+++ src/axios.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+// Create an Axios instance
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
+
+// Add a request interceptor to include JWT token if present
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
